5a715256f0e1e134df23360dc8b42c5b8f09ecba
lucialeitessanchez <lucialeitessanchez@gmail.com>
2021-12-09 13:03:24 -0300

    Se agrega bundle WhiteOctoberTCPDFBundle, se corrige regla de ruteo, modificaciones vs

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new BienesBundle\BienesBundle(),
+            new WhiteOctober\TCPDFBundle\WhiteOctoberTCPDFBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
