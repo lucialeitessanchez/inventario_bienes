a2343508495cf4516659206204c98f6a7cfcf1d1
lucialeitessanchez <lucialeitessanchez@gmail.com>
2022-05-20 11:04:01 -0300

    habia borrado la fecha de adquisicion, la agrego de nuevo

--- src/BienesBundle/Controller/DefaultController.php
+++ src/BienesBundle/Controller/DefaultController.php
@@ -99,7 +99,7 @@ class DefaultController extends Controller
         $txt16="\nCaracteristica: ".$bien->getTipo()." ".$bien->getRama();
 
         //cuerpo del texto
-        $pdf->Write(0, $txt.$txt2.$txt3.$txt4.$txt5b.$txt6.$txt7.$txt9.$txt10.$txt11.$txt12.$txt13.$txt14.$txt15.$txt16, '', 0, '', true, 0, false, false, 0);
+        $pdf->Write(0, $txt.$txt2.$txt3.$txt4.$txt5.$txt5b.$txt6.$txt7.$txt9.$txt10.$txt11.$txt12.$txt13.$txt14.$txt15.$txt16, '', 0, '', true, 0, false, false, 0);
 
         //firmas
         if($responsable->getFuncionario()){ //si es funcionario solo aparece el mismo, no necesita autorizacion
