9f6f980a3f25b7ccf0f10ad06a35b12c93b52373
Vazquez Veronica <vvazquez@santafe.gov.ar>
2022-07-25 13:40:25 -0300

    vistas devolucion, envio reparador, devolucion reparador

--- src/BienesBundle/Controller/VistaController.php
+++ src/BienesBundle/Controller/VistaController.php
@@ -26,14 +26,39 @@ class VistaController extends Controller {
         return $this->render('vista/vista_factura_alta.html.twig');
     }
 
-    public function altaBienAction() {
+    public function ingresoBienAction() {
 
         return $this->render('vista/vista_01_ingreso.html.twig');
     }
-    
+
     public function asignacionBienAction() {
 
         return $this->render('vista/vista_02_asignacion.html.twig');
+    }
+
+    public function prestamoBienAction() {
+
+        return $this->render('vista/vista_03_prestamo.html.twig');
+    }
+
+    public function devolucionBienAction() {
+
+        return $this->render('vista/vista_04_devolucion.html.twig');
+    }
+
+    public function daniadoBienAction() {
+
+        return $this->render('vista/vista_05_daniado.html.twig');
+    }
+
+    public function envioReparacionBienAction() {
+
+        return $this->render('vista/vista_06_envio_reparacion.html.twig');
+    }
+
+    public function devolucionReparadorBienAction() {
+
+        return $this->render('vista/vista_07_devolucion_reparador.html.twig');
     }
 
 }
